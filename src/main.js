449a49f4f7021035d254936dc7012c870ff10db4
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-01-05 18:15:18 -0800

    updating main page

--- src/main.js
+++ src/main.js
@@ -17,7 +17,13 @@ document.querySelector("#app").innerHTML = `
             <p class="intro__dek">
                 We just found them.
 			</p>
-            
+            <br></br>
+            <button id="scroll-button" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="128" height="52" viewBox="0 0 128 52" fill="none">
+                    <path d="M33.7945 14.7931L64 37.2069L94.2055 14.7931M25.1644 51H102.836C116.181 51 127 40.1931 127 26.8621V25.1379C127 11.8069 116.181 1 102.836 1H25.1644C11.8188 1 1 11.8069 1 25.1379V26.8621C1 40.1931 11.8188 51 25.1644 51Z" stroke="#F5C53B" stroke-width="2"/>
+                </svg>
+            </button>
+            <div id="scroll-text">SCROLL DOWN</div>
 		</section>
 
 		<section id="scrolly">
@@ -26,16 +32,16 @@ document.querySelector("#app").innerHTML = `
 
 			<article>
 				<div class="step" data-step="1">
-					<h1>THIS IS A CHICKEN</h1>
+					<h1 id="1-chicken">THIS IS A CHICKEN</h1>
 				</div>
 				<div class="step" data-step="2">
-					<h1>THESE ARE 1,000 CHICKENS</h1>
+					<h1>THIS IS 1,000 CHICKENS</h1>
 				</div>
 				<div class="step" data-step="3">
-					<h1>THESE ARE 10,000 CHICKENS</h1>
+					<h1>THIS IS 10,000 CHICKENS</h1>
 				</div>
 				<div class="step" data-step="4">
-					<h1 class="highlight">THESE ARE 100,000 CHICKENS</h1>
+					<h1>THIS 🟨 IS 100,000 CHICKENS</h1>
 				</div>
                 <div class="step" data-step="5">
 					<h1>There are 10 Billion Chickens raised in the United States every year</h1>
@@ -65,6 +71,13 @@ var step = article.selectAll(".step");
 
 // initialize the scrollama
 var scroller = scrollama();
+var scrollButton = document.getElementById("scroll-button");
+scrollButton.addEventListener("click", function () {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth",
+    });
+}); 
 
 // generic window resize listener event
 function handleResize() {
@@ -72,8 +85,8 @@ function handleResize() {
     var stepH = Math.floor(window.innerHeight * 1);
     step.style("height", stepH + "px");
 
-    var figureHeight = window.innerHeight / 1;
-    var figureMarginTop = (window.innerHeight - figureHeight) / 5;
+    var figureHeight = window.innerHeight / 4;
+    var figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
     figure.style("height", figureHeight + "px").style("top", figureMarginTop + "px");
 
