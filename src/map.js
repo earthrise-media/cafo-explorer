36d4a2cc512df4e7375e128782ec913f1ef1d67c
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-01-09 15:46:28 -0800

    chicken counts and legend

--- src/map.js
+++ src/map.js
@@ -9,14 +9,30 @@ let bbox = null;
 var accessToken = "pk.eyJ1IjoicGxvdGxpbmUiLCJhIjoiY2xmOGo1NW4wMGVtNzNya2UyNnllZGcyciJ9.gUFn8Mj5HQbagkpQWaDqaw";
 document.querySelector("#app").innerHTML = `
 	<main>
-        <div id="control-panel">
-            <div id="cafo-count"></div>
-            <div id="cafo-label"></div>
+        <div class="card control-panel" id="control-panel">
+            <div class="counts-wrapper"> 
+                <div class="counts">
+                    <div class="count-number" id="cafo-count"></div>
+                    <div class="count-label" id="cafo-label"></div>
+                </div>
+                <div class="vertical-line"></div>
+                <div class="counts">
+                    <div class="count-number" id="chicken-count"></div>
+                    <div class="count-label" id="chicken-label"></div>
+                </div>
+            </div>
+            <div class="legend" id="legend" >
+                <div class="legend-label">CAFO - Concentrated Animal Feeding Operation</div>
+                <div class="legend-wrapper"> 
+                    <svg class="legend-label" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="1.5" y="1.5" width="15" height="15" stroke="#F5C53B" stroke-width="3" stroke-linejoin="bevel"/>
+                    </svg>
+                    <div class="legend-label" style="margin-left: 2px;"> - Identified CAFO Complex</div>
+                </div>
+            </div>
         </div>
-        <div id="dynamic-frame">
-        </div>
-        <div id="static-frame">
-        </div>
+        <div id="dynamic-frame"></div>
+        <div id="static-frame"></div>
         <div id="curtain-1">
             <div class="instructions">Hover over an identified CAFO site</div>
         </div>
@@ -57,10 +73,12 @@ const satmap = new mapboxgl.Map({
     interactive: false,
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+// map.addControl(new mapboxgl.NavigationControl());
 map.on("load", () => {
-    document.getElementById("cafo-count").innerHTML = `${cafosVisible()}`;
-    document.getElementById("cafo-label").innerHTML = `Identified 🐓 CAFO Sites Visible`;
+    document.getElementById("cafo-count").innerHTML = `${countVisible()[0]} 🏠`;
+    document.getElementById("cafo-label").innerHTML = `CAFO Complexes Visible`;
+    document.getElementById("chicken-count").innerHTML = `~${countVisible()[1]} 🐓`;
+    document.getElementById("chicken-label").innerHTML = `Chickens Visible`;
     // style the map for the building-extrusion layer to have a hover state that turns the buildings red
     minimap.setPaintProperty("building-extrusion", "fill-extrusion-color", ["case", ["boolean", ["feature-state", "hover"], false], "#ffa201", "#9e8861"]);
 
@@ -80,34 +98,65 @@ map.on("load", () => {
             console.log("Bounding Box: ", bbox);
             moveMiniMap(bbox, polyCentroid);
             // query rendered features on minimap in the building-extrusion layer
-            let queriedBuildings = minimap.queryRenderedFeatures({
-                layers: ["building-extrusion"],
-            });
-            changeBuildingColor(queriedBuildings);
+            // let queriedBuildings = minimap.queryRenderedFeatures({
+            //     layers: ["building-extrusion"],
+            // });
+            // changeBuildingColor(queriedBuildings);
         });
 
         map.on("mouseleave", "poly-fill", function () {
             document.getElementById("static-frame").classList.remove("active");
         });
         map.on("zoomend", function () {
-            document.getElementById("cafo-count").innerHTML = `${cafosVisible()}`;
+            document.getElementById("cafo-count").innerHTML = `${countVisible()[0]} 🏠`;
+            document.getElementById("chicken-count").innerHTML = `~${countVisible()[1]} 🐓`;
             // document.getElementById("cafo-label").innerHTML = `Identified 🐓 CAFOs Visible`;
         });
         map.on("moveend", function () {
-            document.getElementById("cafo-count").innerHTML = `${cafosVisible()}`;
+            document.getElementById("chicken-count").innerHTML = `${countVisible()[0]} 🏠`;
+            document.getElementById("chicken-count").innerHTML = `~${countVisible()[1]} 🐓`;
         });
     }
 });
+// from https://www.nass.usda.gov/Charts_and_Maps/Poultry/brlmap.php
+const stateCounts = {
+    AL: 1204700000,
+    AR: 1051400000,
+    MS: 670700000,
+    GA: 1308400000,
+    SC: 235600000,
+    NC: 976200000,
+    total: 5447000000,
+};
 
-// begin functions
-function cafosVisible() {
+function countVisible() {
+    // add a handler that changes font size based on the number of points visible
     let pointsVisible = map.queryRenderedFeatures({
         layers: ["points-1"],
     });
     let polysVisible = map.queryRenderedFeatures({
         layers: ["poly-outline"],
     });
-    return Math.max(pointsVisible.length, polysVisible.length);
+    let cafoCount = Math.max(pointsVisible.length, polysVisible.length);
+    // yearly state slaughter count, multiplying by .13 and then multiplying again by the ratio of polygons in a view to polygons in the state
+    let chickenCount = Math.round(stateCounts.total * 0.13 * (cafoCount / 16372));
+    // make the chicken count only return 3 digits with the number spelled out
+    if (chickenCount >= 10000000 && chickenCount < 1000000000) {
+        // For millions
+        chickenCount = `${Math.round(chickenCount / 1000000)} million`;
+    } else if (chickenCount >= 1000000000) {
+        // For billions
+        chickenCount = `${Math.round(chickenCount / 1000000000)} billion`;
+    }
+
+    // if either number is more than 1000, add a comma
+    if (cafoCount >= 1000) {
+        cafoCount = cafoCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    if (chickenCount >= 1000) {
+        chickenCount = chickenCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    return [cafoCount, chickenCount];
 }
 function getStaticMap(lat, lon) {
     staticURL = `https://api.mapbox.com/styles/v1/plotline/clqybizqa00f101rj8l32czwr/static/${lon},${lat},15,0,0/${staticFrameDimensions[0]}x${staticFrameDimensions[1]}@2x?access_token=${accessToken}&attribution=false&logo=false`;
@@ -182,13 +231,15 @@ function changeBuildingColor(queried) {
 function queryBuildingFeatures(bbox) {
     // based on the tilequery constraints, you can only choose a point and query within a radius. For this to be functional, we'd need a large radius and then take all those features and determine which are within the bounding box
     const tilequeryUrl = `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/${bbox[0]},${bbox[1]}.json?radius=1000?layers=building&access_token=${accessToken}`;
-    console.log(tilequeryUrl)
+    console.log(tilequeryUrl);
     fetch(tilequeryUrl)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             console.log("Building Features:", data.features);
         })
-        .catch(error => {
+        .catch((error) => {
             console.error("Error querying building features:", error);
         });
 }
+
+// If we simply neglect layers and breeders, we can get a moderately conservative estimate of chickens in any given map view by taking the yearly state slaughter count, multiplying by .13 and then multiplying again by the ratio of polygons in a view to polygons in the state.
